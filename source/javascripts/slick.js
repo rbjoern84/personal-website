3440153063ed6aeb1ba0c1fcb15b47a26a5e1e97
Björn Richter <br@magiclabs.de>
2017-07-10 20:46:09 +0200

    Initializes slick carousel

---
+++ source/javascripts/slick.js
@@ -0,0 +1,23 @@
+$(document).ready( function(){
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    autoplay: false,
+    arrows: false,
+    speed: 500,
+    cssEase: 'ease',
+    mobileFirst: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    centerPadding: '0',
+    slidesToShow: 1,
+
+    responsive: [{
+
+      breakpoint: 768,
+      settings: {
+        arrows: true
+      }
+    }]
+  });
+});
